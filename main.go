5677b09d61151d260d8107f8094ca7dae9377b51
James Parrish <jparrishjr@gmail.com>
2024-01-19 08:37:39 -0600

    update

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
 
-}
+	hand.print()
+	remainingCards.print()
 
-func newCard() string {
-	return "Five of Diamonds"
 }
